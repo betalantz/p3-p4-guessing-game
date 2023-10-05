a6875af23fd636eb1473051932b9eb34117d7a72
betalantz <betalantz@gmail.com>
2023-10-05 11:35:25 -0700

    create User in tmp.py

--- 4_client_server_db_jwt/server/tmp.py
+++ 4_client_server_db_jwt/server/tmp.py
@@ -1,6 +1,6 @@
 from app import app
 from db import db
-from models import Game, Round
+from models import Game, Round, User
 from sqlalchemy import delete
 
 
@@ -8,8 +8,12 @@ def seed():
     """Seed tables"""
     db.session.execute(delete(Round))
     db.session.execute(delete(Game))
-    game1 = Game(difficulty="easy", range_min=1, range_max=10)
-    game2 = Game(difficulty="hard", range_min=1, range_max=100)
+    db.session.execute(delete(User))
+    user1 = User(name="user1", password="flatiron")
+    db.session.add(user1)
+    db.session.commit()
+    game1 = Game(user=user1, difficulty="easy", range_min=1, range_max=10)
+    game2 = Game(user=user1, difficulty="hard", range_min=1, range_max=100)
     db.session.add_all([game1, game2])
     db.session.commit()
     db.session.add(
