5b7db92f11c98c98e98da6718d7cba12117eb7ab
betalantz <betalantz@gmail.com>
2023-10-18 16:44:28 -0700

    adds tests for /games/:id

--- 2_client_server_no_db/server/testing/view_test.py
+++ 2_client_server_no_db/server/testing/view_test.py
@@ -85,3 +85,46 @@ class TestGamesById:
             )
             assert response.status_code == 200
             assert response.json["is_over"] == True
+
+    def test_games_by_id_patch_invalid_id(self):
+        """
+        PATCH route for '/games/<string:game_id>' returns a 404 error if the game id is invalid.
+        """
+        with app.test_client() as client:
+            response = client.patch("/games/invalid_id", json={"guess": 1})
+            assert response.status_code == 404
+            assert response.json["message"] == "Game invalid_id not found."
+
+    def test_games_by_id_patch_high_guess(self, test_game):
+        """
+        PATCH route for '/games/<string:game_id>' returns round status 'high' if the guess is too high.
+        """
+        with app.test_client() as client:
+            response = client.patch(
+                f"/games/{test_game.id}", json={"guess": test_game.secret_number + 1}
+            )
+            assert response.status_code == 200
+            assert response.json["rounds"][-2]["status"] == "high"
+
+    def test_games_by_id_patch_low_guess(self, test_game):
+        """
+        PATCH route for '/games/<string:game_id>' returns round status 'low' if the guess is too low.
+        """
+        with app.test_client() as client:
+            response = client.patch(
+                f"/games/{test_game.id}", json={"guess": test_game.secret_number - 1}
+            )
+            assert response.status_code == 200
+            assert response.json["rounds"][-2]["status"] == "low"
+
+    def test_games_by_id_delete(self, test_game):
+        """
+        has a DELETE route for '/games/<string:game_id>'.
+        """
+        with app.test_client() as client:
+            response = client.delete(f"/games/{test_game.id}")
+            assert response.status_code == 204
+            assert response.data == b""
+            next_response = client.get("/games")
+            assert next_response.status_code == 200
+            assert next_response.json == []
